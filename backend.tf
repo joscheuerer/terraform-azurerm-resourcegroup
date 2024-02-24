e215cbd46566f8b6b5ba2beac179943cf10d449b
Johannes Scheuerer <joscheuerer@gmail.com>
2024-02-24 10:07:18 +0100

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+#blob storage container for state file
+backend "azurerm" {
+  resource_group_name = "jo-new"
+  storage_account_name = "unipertest"
+  container_name = "jotfstate-test"
+  key = "terraform.tfstate"
+  access_key = "REPLACE_WITH_ACCESS_KEY"
+}
