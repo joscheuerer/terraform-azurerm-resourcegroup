b0e14d4a93182e306ef29fdc3b570c879d41595a
Johannes Scheuerer <joscheuerer@gmail.com>
2024-02-24 10:24:48 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,6 +5,6 @@ terraform {
     storage_account_name = "unipertest"
     container_name = "jotfstate-test"
     key = "terraform.tfstate"
-    access_key = "ribzEVqJp6EjFD8ClH8HwLTBtpGndKvyC50LSd1HqWcEjYEJ1Ow2ck5FtZfMbkdyKZtr5a9Icw15+AStuMwnNg=="
+    access_key = "REPLACE_WITH_ACCESS_KEY"
   }
 }
